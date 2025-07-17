da570ab62aeb62cd806ce8274eb85a4d8cba56f5
andrewgomescode <andrewgomes.brc@gmail.com>
2025-07-17 04:37:46 -0300

    Enhanced player movement functionality by adding animation support and direction flipping in PlayerMovement script. Updated SampleScene with a new Animator component and adjusted player position settings.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
     public Rigidbody2D rb;
+    public Animator anim;
+    public int facingDirection = 1;
     
     // FixedUpdate is called 50x frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        
+        // condição para inverter a direção do personagem (Flip)
+        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        // animação do personagem
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
 
         // Normaliza a direção do movimento (corrige o problema de andar mais rápido na diagonal)
         Vector2 direction = new Vector2(horizontal, vertical);
@@ -19,5 +31,12 @@ public class PlayerMovement : MonoBehaviour
             direction = direction.normalized;
 
         rb.linearVelocity = direction * speed;
+    }
+
+    // função para inverter a direção do personagem (Flip)
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
